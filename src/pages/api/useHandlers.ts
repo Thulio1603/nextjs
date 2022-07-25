618c92087d5550d0e92ba934e90378cd1f7f0bb8
thfurtado <thfurtado@uolinc.com>
2022-07-25 20:29:05 -0300

    feat: exemple next-connect

--- src/pages/api/useHandlers.ts
+++ src/pages/api/useHandlers.ts
@@ -3,7 +3,7 @@ import handler from './handler'
 export default handler()
   .get(async (req, res) => {
     const { cookie } = req.headers;
-
+    console.log(cookie)
     try {
       res.status(200).json('data')
     } catch (error: any) {
@@ -17,5 +17,13 @@ export default handler()
       res.status(200).json('data')
     } catch (error: any) {
       res.status(error?.response?.status || 404).json({ message: error?.message })
+    }
+  })
+  .delete(async (req, res) => {
+    const { body } = req
+    try {
+      res.status(200).send('data')
+    } catch (error) {
+      res.status(500).json({ message: error.message })
     }
   })
