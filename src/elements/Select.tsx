4b22601542b01fb0bdfb89ea28f99f8aad04c1a6
thulio1603 <thulio1603@gmail.com>
2022-10-01 16:04:38 -0300

    adicionando @radix-ui/react-select

---
+++ src/elements/Select.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import * as SelectPrimitive from '@radix-ui/react-select'
+import { ChildrenProps } from '../model/reactFc'
+import { styled } from '../theme/stitches.config'
+
+type SelectProps = SelectPrimitive.SelectProps & ChildrenProps
+
+const Select = ({children, ...rest}: SelectProps) =>
+  <SelectPrimitive.Root {...rest}>{children}</SelectPrimitive.Root>
+
+Select.Trigger = styled(SelectPrimitive.Trigger, { 
+  background: 'black'
+})
+
+Select.Content = styled(SelectPrimitive.Content, { 
+  background: 'purple'
+})
+
+Select.Item = styled(SelectPrimitive.Item, { 
+  background: 'red'
+})
+
+export default Select
